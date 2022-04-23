8690570ff003463b3d9bc33fa76aa623fb14c0ce
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-04-23 19:07:46 -0300

    refactoring routing

---
+++ src/Controller/Persistencia.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    private $entityManager;
+    
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator ())->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        $descricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_SPECIAL_CHARS
+        );
+
+        $curso = new Curso();
+        $curso->setDescricao($descricao);
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+
+        header('Location: /listar-cursos', true, 302);
+    }
+}
--- view/cursos/formulario.php
+++ view/cursos/formulario.php
@@ -1,5 +1,5 @@
 <?php include __DIR__ . '/../inicio-html.php'; ?>
-    <form action="">
+    <form action="/salvar-curso" method="POST">
         <div class="form-group">
             <label for="descricao">Descrição</label>
             <input type="text" id="descricao" name="descricao" class="form-control">
